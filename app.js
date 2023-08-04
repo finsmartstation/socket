3975204fa47d26c1b14dceb2835c212d47f7f84b
fssvibin <fss.vibin@gmail.com>
2023-08-04 17:01:53 +0530

    changes upto_04-08-2023

--- app.js
+++ app.js
@@ -7703,7 +7703,7 @@ io.sockets.on('connection',async function (socket) {
     })
     socket.on('test_changes',async function(data){
       socket.join('test_changes');
-      io.sockets.in('test_changes').emit('test_changes',{status: true, statuscode: 200, message: "last changes affected upto 3-08-2023 (2)"});
+      io.sockets.in('test_changes').emit('test_changes',{status: true, statuscode: 200, message: "last changes affected upto 4-08-2023 (2)"});
       socket.leave('test_changes');
     });
     socket.on('private_chat_export_data',async function(data){
